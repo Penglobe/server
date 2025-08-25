06154bcb5fb3ae6115c6a17b323b8b1090be23cf
heejung003 <heejung003@gmail.com>
2025-08-25 17:55:43 +0900

    ✨point 기능 추가

--- src/main/java/com/penglobe/server/repository/PointsLedgerRepository.java
+++ src/main/java/com/penglobe/server/repository/PointsLedgerRepository.java
@@ -1,7 +1,12 @@
 package com.penglobe.server.repository;
 
 import com.penglobe.server.domain.ledger.PointsLedger;
+import com.penglobe.server.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PointsLedgerRepository extends JpaRepository<PointsLedger, Long> {
+    //사용자 id로 포인트 사용내역 조회
+    List<PointsLedger> pointListById(User user);
 }
