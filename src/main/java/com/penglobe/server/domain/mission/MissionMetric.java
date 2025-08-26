abfb001c4c474cb890fdaa2cc75c4d7eaee3f550
dnjswjd222 <bnm673@naver.com>
2025-08-26 16:28:22 +0900

    ✨ feat: 미션 엔티티 추가

---
+++ src/main/java/com/penglobe/server/domain/mission/MissionMetric.java
@@ -0,0 +1,7 @@
+package com.penglobe.server.domain.mission;
+
+public enum MissionMetric {
+    WALK_CO2_KG,        // 도보 절감량 (kg)
+    DIET_CO2_KG,        // 식단 절감량 (kg)
+    ATTEND_TOTAL_DAYS  // 누적 출석 (days)
+}
