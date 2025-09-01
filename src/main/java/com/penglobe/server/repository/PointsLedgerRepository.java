116f8f8652eda87b273972bc35b9082e190becb6
heejung003 <heejung003@gmail.com>
2025-09-01 12:26:46 +0900

    ♻️포인트주문내역수정

--- src/main/java/com/penglobe/server/repository/PointsLedgerRepository.java
+++ src/main/java/com/penglobe/server/repository/PointsLedgerRepository.java
@@ -8,17 +8,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
+
 public interface PointsLedgerRepository extends JpaRepository<PointsLedger, Long> {
-    //사용자 id로 포인트 사용내역 조회
-    @Query("SELECT p FROM PointsLedger p WHERE p.user = :user AND p.eventDate BETWEEN :from AND :to ORDER BY p.eventDate DESC")
-    List<PointsLedger> findByUserAndEventDateBetweenOrderByEventDateDesc(
-            @Param("user") User user,
-            @Param("from") LocalDate from,
-            @Param("to") LocalDate to
-    );
-    //현재 잔액 조회
-    PointsLedger findTopByUserOrderByEventDateDesc(User user);
+
+    // 사용자별 전체 포인트 내역 조회 (최근 내역이 먼저)
+    List<PointsLedger> findByUserOrderByCreatedAtDesc(User user);
+
+    // 최신 레저 조회
+    PointsLedger findTopByUserOrderByCreatedAtDesc(User user);
 }
