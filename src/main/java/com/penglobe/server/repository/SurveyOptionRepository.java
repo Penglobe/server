89b24f46286911bf6d639ba93c2354b319c69e5e
heejung003 <heejung003@gmail.com>
2025-09-01 12:00:06 +0900

    �ì¤문조사 레포지토리 수정정

--- src/main/java/com/penglobe/server/repository/SurveyOptionRepository.java
+++ src/main/java/com/penglobe/server/repository/SurveyOptionRepository.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 public interface SurveyOptionRepository extends JpaRepository<SurveyOption, Long> {
     //선택지 관리, co2값 조회 등
     //선택지 추가/삭제는 안 할거지만 추후 수정할 걸 생각해서
-    @Query("SELECT MAX(o.co2) FROM SurveyOption o WHERE o.surveyItem.itemId = :itemId")
+    @Query("SELECT MAX(o.co2kg) FROM SurveyOption o WHERE o.surveyItem.itemId = :itemId")
     Double findMaxCo2ByItemId(@Param("itemId") Long itemId);
 
     Optional<SurveyOption> findBySurveyItem_ItemIdAndValue(Long itemId, Integer value);
