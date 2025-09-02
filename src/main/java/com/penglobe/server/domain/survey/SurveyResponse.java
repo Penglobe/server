a902390c41fb81f3a3008db6c27e78ac72a62446
heejung003 <heejung003@gmail.com>
2025-09-02 09:55:19 +0900

    ♻️설문조사 엔티티 수정

--- src/main/java/com/penglobe/server/domain/survey/SurveyResponse.java
+++ src/main/java/com/penglobe/server/domain/survey/SurveyResponse.java
@@ -21,7 +21,7 @@ import java.util.List;
 public class SurveyResponse extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long surveyResponseId;
 
     @Column(nullable = false)
     private Long userId;
