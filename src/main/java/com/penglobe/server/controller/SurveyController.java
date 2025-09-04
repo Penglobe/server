f92ed970231304004ac69860ea51c5dc1527762b
heejung003 <heejung003@gmail.com>
2025-09-04 17:38:04 +0900

    ✨설문조사페이지

--- src/main/java/com/penglobe/server/controller/SurveyController.java
+++ src/main/java/com/penglobe/server/controller/SurveyController.java
@@ -32,8 +32,13 @@ public class SurveyController {
     //설문 제출
     //총 Co2 계산 -> top3
     @Operation(summary = "설문 제출/Top3", description = "사용자가 설문을 제출하면 총 CO2와 Top3 항목을 계산합니다.")
-    @PostMapping("/submit")
-    public ResponseEntity<?> submitSurvey(@RequestBody SurveySubmitRequestDTO dto) {
+    @PostMapping("/submit/{userId}")
+    public ResponseEntity<?> submitSurvey(@RequestBody SurveySubmitRequestDTO dto, @PathVariable Long userId) {
+        dto.setUserId(userId);
+
+        System.out.println("받은 userId " + userId);
+        System.out.println("받은 answer " + dto.getAnswer()) ;
+
    SurveyResultDTO result = surveyService.submitSurvey(dto);
     return ResponseEntity.ok(ApiResponse.success(result));
     }
