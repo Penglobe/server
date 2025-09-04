a20f8e6eb2e2d0908447e56e75ddecc634096d83
heejung003 <heejung003@gmail.com>
2025-09-04 14:01:50 +0900

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,21 +3,21 @@
 -- =====================
 -- 엔티티에 @CreationTimestamp가 있어도, Hibernate가 엔티티를 통해 INSERT할 때만 자동으로 채워짐.
 -- data.sql은 Hibernate/JPA를 거치지 않고 DB에 직접 실행되기 때문에 created_at, updated_at 안주면 null로 들어가 버림
-INSERT INTO users (type, email, password_hash, nickname, region_id, total_point, profile_id, kakao_id, is_profile_complete, last_week_rank, created_at, updated_at)
-VALUES ('USER', 'user1@example.com', 'hashed_pw1', 'Alice', 101, 100, NULL, NULL, 1, NULL, NOW(), NOW());
-
-INSERT INTO users (type, email, password_hash, nickname, region_id, total_point, profile_id, kakao_id, is_profile_complete, last_week_rank, created_at, updated_at)
-VALUES ('USER', 'user2@example.com', 'hashed_pw2', 'Bob', 102, 50, NULL, NULL, 0, NULL, NOW(), NOW());
+-- INSERT INTO users (type, email, password_hash, nickname, region_id, total_point, profile_id, kakao_id, is_profile_complete, last_week_rank, created_at, updated_at)
+-- VALUES ('USER', 'user1@example.com', 'hashed_pw1', 'Alice', 101, 100, NULL, NULL, 1, NULL, NOW(), NOW());
+--
+-- INSERT INTO users (type, email, password_hash, nickname, region_id, total_point, profile_id, kakao_id, is_profile_complete, last_week_rank, created_at, updated_at)
+-- VALUES ('USER', 'user2@example.com', 'hashed_pw2', 'Bob', 102, 50, NULL, NULL, 0, NULL, NOW(), NOW());
 
 -- ======================
 -- 2. UserCounters 더미 데이터
 -- ======================
 -- user_id는 users 테이블의 PK(auto_increment)와 동일하게
 -- total_* 값은 모두 기본 0으로 시작
-INSERT INTO user_counters (user_id, total_distance_co2kg, total_diet_co2kg, total_survey_co2kg,
-                           attendance_total_days, attendance_streak_days, attendance_month_days,
-                           last_attendance_date, longest_attendance_streak,
-                           created_at, updated_at)
-VALUES
-    (1, 0, 0, 0, 0, 0, 0, NULL, 0, NOW(), NOW()), -- Alice용 카운터
-    (2, 0, 0, 0, 0, 0, 0, NULL, 0, NOW(), NOW()); -- Bob용 카운터
+-- INSERT INTO user_counters (user_id, total_distance_co2kg, total_diet_co2kg, total_survey_co2kg,
+--                            attendance_total_days, attendance_streak_days, attendance_month_days,
+--                            last_attendance_date, longest_attendance_streak,
+--                            created_at, updated_at)
+-- VALUES
+--     (1, 0, 0, 0, 0, 0, 0, NULL, 0, NOW(), NOW()), -- Alice용 카운터
+--     (2, 0, 0, 0, 0, 0, 0, NULL, 0, NOW(), NOW()); -- Bob용 카운터
