771e08fc4594994002cb9774b4fca2c6370a561f
heejung003 <heejung003@gmail.com>
2025-09-08 17:12:14 +0900

    ✨feat : 설문조사 차트

---
+++ src/main/java/com/penglobe/server/domain/survey/DailyStatistics.java
@@ -0,0 +1,35 @@
+package com.penglobe.server.domain.survey;
+
+import com.penglobe.server.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+@Entity
+@Table(name="survey_statistic")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DailyStatistics extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long statistics_id;
+    private LocalDate date; //해당 일자
+    private int dayOfWeek; //월~일
+
+    @Column(nullable = true)
+    private Long userId;
+
+    private double statisticsTotalCo2kg;
+    private int yearWeek;
+
+
+    @Column(nullable = true)
+    private Integer userCount;
+}
